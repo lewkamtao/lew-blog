a5b908c865d5af3d656b525ebdf8205a01da1772
卢锦涛 <div@kamtao.com>
2022-12-12 21:32:34 +0800

    fix: 路径

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "lew_blog",
         exec_mode: "cluster",
         instances: "max", // Or a number of instances
-        script: "./output/server/index.mjs",
+        script: "./.output/server/index.mjs",
         args: "start",
         env: {
             NODE_ENV: "production",
