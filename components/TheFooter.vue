447d8616691e6f990cb18df8eaeed832e312ac1c
lew <div@kamtao.com>
2024-11-21 20:29:58 +0800

    feat: 新增下班时间

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
 import { useNavigatorLanguage } from "@vueuse/core";
+import dayjs from "dayjs";
+
+let now = dayjs().subtract(9.5, "hour");
+// 打印当前时间 倒推 9 个半小时之前的时间
+const goHomeTime = ref(now.format("YYYY-MM-DD HH:mm:ss"));
+
+// 每秒更新一次时间
+onMounted(() => {
+  setInterval(() => {
+    now = dayjs().subtract(9.5, "hour");
+    goHomeTime.value = now.format("YYYY-MM-DD HH:mm:ss");
+  }, 1000);
+});
+
 const blog = useBlog();
 const route = useRoute();
 const { language } = useNavigatorLanguage();
@@ -12,11 +26,12 @@ const { language } = useNavigatorLanguage();
       <a target="_blank" href="https://beian.miit.gov.cn/">粤ICP备18056223号</a>
     </div>
     <div>
-      <span> Current version 1.12.0 </span>
-      <span> PATH {{ route.path }} </span>
+      <span> Current Version 1.12.0 </span>
+      <span> Path {{ route.path }} </span>
       <span> Language {{ language }} </span>
       <span> Series {{ blog.seriesTotal }} </span>
       <span> Article {{ blog.articleTotal }} </span>
+      <span> 9.5 Hours Ago {{ goHomeTime }} </span>
     </div>
   </div>
 </template>
